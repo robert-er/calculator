64dabe2b745abf8867c1572f94239563d540bbde
robert-er <robert.roza.pl@gmail.com>
2020-05-28 04:13:35 +0200

    Calc0.2

--- src/com/kodilla/Main.java
+++ src/com/kodilla/Main.java
@@ -3,6 +3,29 @@ package com.kodilla;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a=3;
+        int b=5;
+        double aForSub = 4.5;
+        double bForSub = 5.5;
+        SimpleCalculator calculator = new SimpleCalculator(a,b);
+        SimpleCalculator calcSub = new SimpleCalculator(aForSub,bForSub);
+
+        int addResult = calculator.addAtoB();
+        double subResult = calcSub.subAfromB();
+
+        System.out.println("obiekt calculator:");
+        System.out.println(calculator.getA());
+        System.out.println(calculator.getB());
+        System.out.println(calculator.getAForSub());
+        System.out.println(calculator.getBForSub());
+
+        System.out.println("obiekt calcSub:");
+        System.out.println(calcSub.getA());
+        System.out.println(calcSub.getB());
+        System.out.println(calcSub.getAForSub());
+        System.out.println(calcSub.getBForSub());
+
+        System.out.println("Suma liczb " + calculator.getA() + " i " + calculator.getB() + " = " + addResult);
+        System.out.println("Roznica liczb " + calcSub.getBForSub() + " - " + calcSub.getAForSub() + " = " + subResult);
     }
 }
--- src/com/kodilla/SimpleCalculator.java
+++ src/com/kodilla/SimpleCalculator.java
@@ -1,4 +1,42 @@
 package com.kodilla;
 
 public class SimpleCalculator {
+    private int a;
+    private int b;
+    private double aForSub;
+    private double bForSub;
+
+    public SimpleCalculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public SimpleCalculator(double aForSub, double bForSub) {
+        this.aForSub=aForSub;
+        this.bForSub=bForSub;
+    }
+
+    public int addAtoB () {
+        return this.a+this.b;
+    }
+
+    public double subAfromB () {
+        return this.bForSub-this.aForSub;
+    }
+
+    public int getA() {
+        return this.a;
+    }
+
+    public int getB() {
+        return this.b;
+    }
+
+    public double getAForSub() {
+        return this.aForSub;
+    }
+
+    public double getBForSub() {
+        return this.bForSub;
+    }
 }
